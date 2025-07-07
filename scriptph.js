1cc7f6e52120a416be41e8cb1aa26d897ee93d77
Pirros Nikopoulos <mai25039@uom.edu.gr>
2025-07-07 22:43:26 +0300

    Update scriptph.js

--- scriptph.js
+++ scriptph.js
@@ -73,15 +73,15 @@ function deleteTask(index){
 
 // delete all tasks function
 deleteAllBtn.onclick = ()=>{
-  let getLocalStorageData = localStorage.getItem("New Todo"); //getting localstorage
-  if(getLocalStorageData == null){ //if localstorage has no data
-    listArray = []; //create a blank array
+  let getLocalStorageData = localStorage.getItem("New Todo"); 
+  if(getLocalStorageData == null){ 
+    listArray = []; 
   }else{
-    listArray = JSON.parse(getLocalStorageData);  //transforming json string into a js object
-    listArray = []; //create a blank array
+    listArray = JSON.parse(getLocalStorageData);  
+    listArray = []; 
   }
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); //set the item in localstorage
-  showTasks(); //call the showTasks function
+  localStorage.setItem("New Todo", JSON.stringify(listArray)); 
+  showTasks(); 
 }
 
 function toggleCheckbox(index) {
@@ -97,38 +97,3 @@ function toggleCheckbox(index) {
 
 
 
-/*function showTasks1(){
-  let getLocalStorageData = localStorage.getItem("New Topic");
-  if(getLocalStorageData == null){
-    listArray = [];
-  }else{
-    listArray = JSON.parse(getLocalStorageData); 
-  }
-  const pendingTasksNumb = document.querySelector(".pendingTasks");
-  pendingTasksNumb.textContent = listArray.length; //passing the array length in pendingtask
-  if(listArray.length > 0){ //if array length is greater than 0
-    deleteAllBtn.classList.add("active"); //active the delete button
-  }else{
-    deleteAllBtn.classList.remove("active"); //unactive the delete button
-  }
-  let newLiTag = "";
-  listArray.forEach((element, index) => {
-    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
-  })
-  TopicList.innerHTML = newLiTag; //adding new li tag inside ul tag
-  inputBox.value = ""; //once task added leave the input field blank
-}
-addBtn.onclick = ()=>{ //when user click on plus icon button
-  let userEnteredValue = inputBox.value; //getting input field value
-  let getLocalStorageData = localStorage.getItem("New Topic"); //getting localstorage
-  if(getLocalStorageData == null){ //if localstorage has no data
-    listArray = []; //create a blank array
-  }else{
-    listArray = JSON.parse(getLocalStorageData);  //transforming json string into a js object
-  }
-  listArray.push(userEnteredValue); //pushing or adding new value in array
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); //transforming js object into a json string
-  
-  showTasks1(); //calling showTask function
-  addBtn.classList.remove("active"); //unactive the add button once the task added
-}*/
